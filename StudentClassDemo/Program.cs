ea3920a0e76616064e6d5451f6a341d6d795c3dd
Quentin-D-NM <qdye2001@gmail.com>
2021-06-30 15:51:55 -0600

    Initial Commit

---
+++ StudentClassDemo/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace StudentClassDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Student molly = new Student("Molly Cat", 1234);
+
+            Console.WriteLine("molly's Id Number: {0}", molly.GetidNumber());
+            Console.WriteLine("molly's Name: {0}", molly.GetName());
+
+            Student bella = new Student("Bella");
+
+            Console.WriteLine("bella's Id Number: {0}", bella.GetidNumber());
+            Console.WriteLine("bella's Name: {0}", bella.GetName());
+
+            Student kitkat = new Student("Kitkat");
+
+            Console.WriteLine("kitkat's Id Number: {0}", kitkat.GetidNumber());
+            Console.WriteLine("kitkat's Name: {0}", kitkat.GetName());
+
+
+        }
+    }
+}
